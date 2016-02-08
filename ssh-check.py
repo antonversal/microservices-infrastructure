9cca9ea9c2d28a3d41a33c57f2ff935c08c7138e
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-02-08 14:13:32 -0600

    Moved logic to try block, added another exception

--- ssh-check.py
+++ ssh-check.py
@@ -5,50 +5,62 @@ import shlex
 import subprocess
 import sys
 
-# get the hosts
 try:
-    hosts = subprocess.check_output(shlex.split("plugins/inventory/terraform.py --hostfile"))
+    # get the hosts
+    cmd = shlex.split("plugins/inventory/terraform.py --hostfile")
+    hosts = subprocess.check_output(cmd)
+
+    # loop through IPs
+    hosts = dict(line.split() for line in hosts.split("\n")
+                 if line != '' and not line.startswith("#"))
+    tainted = []
+
+    # for each ip, try to connect
+    for host, name in hosts.iteritems():
+        cmd = "ssh root@{} -oBatchMode=yes -C 'echo test' 2&>1".format(host)
+        ssh = subprocess.Popen(shlex.split(cmd.format(host)),
+                               stderr=subprocess.PIPE)
+        _, err = ssh.communicate()
+
+        # if error, parse stderr
+        if ssh.returncode != 0 and 'permission denied' in err.lower():
+
+            # load in tfstate directly to get module name
+            with open('terraform.tfstate') as json_file:
+                print("loaded file:", json_file)
+                state = json.load(json_file)
+                cmd = "plugins/inventory/terraform.py --host {}".format(name)
+                meta = subprocess.check_output(shlex.split(cmd))
+                meta = json.loads(meta)
+                for module in state['modules']:
+                    for key, resource in module['resources'].items():
+                        primary = resource['primary']
+
+                        # because
+                        if 'attributes' in primary:
+                            if 'ipv4_address' in primary['attributes']:
+                                address = primary['attributes']['ipv4_address']
+                                if address == meta['ipv4_address']:
+                                    tainted.append((module['path'][-1], key))
+
+    # if any hosts are tainted, reapply and exit 1
+    if len(tainted) == 0:
+        print("no ssh permission errors found, no reapply required")
+        sys.exit(0)
+    else:
+        for name, resource in tainted:
+            cmd = "terraform taint -module={} {}".format(name, resource)
+            print(cmd)
+            ret_code = subprocess.call(shlex.split(cmd))
+            if ret_code != 0:
+                print("subprocess exited with nonzero")
+                sys.exit(1)
+
+        subprocess.call(shlex.split("terraform apply"))
+        sys.exit(0)
 except subprocess.CalledProcessError as e:
-    print("getting data from terraform.py failed, here is the error: ", e)
+    print("error calling subprocess:\n", e)
     sys.exit(1)
-
-# loop through IPs
-hosts = dict(line.split() for line in hosts.split("\n") if line != '' and not line.startswith("#"))
-tainted = []
-
-# for each ip, try to connect
-for host, name in hosts.iteritems():
-    cmd = shlex.split("ssh root@%s -o NumberOfPasswordPrompts=0 -C 'echo test' 2&>1" % host)
-    ssh = subprocess.Popen(cmd, stderr=subprocess.PIPE)
-    _, err = ssh.communicate()
-
-    # if error, parse stderr
-    if ssh.returncode != 0 and 'permission denied' in err.lower():
-
-        # load in tfstate directly to get module name
-        with open('terraform.tfstate') as json_file:
-            state = json.load(json_file)
-            meta = subprocess.check_output(shlex.split('plugins/inventory/terraform.py --host {}'.format(name)))
-            meta = json.loads(meta)
-            for module in state['modules']:
-                for key, resource in module['resources'].items():
-                    if 'attributes' in resource['primary']:
-                        if 'ipv4_address' in resource['primary']['attributes']:
-                            if meta['ipv4_address'] == resource['primary']['attributes']['ipv4_address']:
-                                tainted.append((module['path'][-1], key))
-
-# if any hosts are tainted, reapply and exit 1
-if len(tainted) == 0:
-    print("no ssh permission errors found, no reapply required")
-    sys.exit(0)
-else:
-    for name, resource in tainted:
-        cmd = shlex.split("terraform taint -module={} {}".format(name, resource))
-        print(cmd)
-        ret_code = subprocess.call(cmd)
-        if ret_code != 0:
-            print("subprocess exited with nonzero")
-            sys.exit(0)
-
-    subprocess.call(shlex.split("terraform apply"))
+except IOError as e:
+    print("error opening file:\n", e)
     sys.exit(1)
