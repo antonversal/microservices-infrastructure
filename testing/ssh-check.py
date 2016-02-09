cc3ef482ff306ba3a6d10cbc937728653e41093b
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-02-08 18:00:08 -0600

    Refactored code as Brian suggested

--- testing/ssh-check.py
+++ testing/ssh-check.py
@@ -2,65 +2,70 @@
 from __future__ import print_function
 import json
 import shlex
-import subprocess
+from subprocess import Popen, PIPE
 import sys
 
-try:
+
+def main():
     # get the hosts
-    cmd = shlex.split("plugins/inventory/terraform.py --hostfile")
-    hosts = subprocess.check_output(cmd)
+    _, hosts, _ = call_cmd("plugins/inventory/terraform.py --hostfile")
 
     # loop through IPs
     hosts = dict(line.split() for line in hosts.split("\n")
                  if line != '' and not line.startswith("#"))
-    tainted = []
 
-    # for each ip, try to connect
+    # connect to the ips, looking for resources to taint
     for host, name in hosts.iteritems():
-        cmd = "ssh root@{} -oBatchMode=yes -C 'echo test' 2&>1".format(host)
-        ssh = subprocess.Popen(shlex.split(cmd.format(host)),
-                               stderr=subprocess.PIPE)
-        _, err = ssh.communicate()
+        cmd = "ssh root@{} -oBatchMode=yes -C 'echo hi' 2&>1".format(host)
+        returncode, _, err = call_cmd(cmd)
 
-        # if error, parse stderr
-        if ssh.returncode != 0 and 'permission denied' in err.lower():
-
-            # load in tfstate directly to get module name
-            with open('terraform.tfstate') as json_file:
-                print("loaded file:", json_file)
-                state = json.load(json_file)
-                cmd = "plugins/inventory/terraform.py --host {}".format(name)
-                meta = subprocess.check_output(shlex.split(cmd))
-                meta = json.loads(meta)
-                for module in state['modules']:
-                    for key, resource in module['resources'].items():
-                        primary = resource['primary']
-
-                        # because
-                        if 'attributes' in primary:
-                            if 'ipv4_address' in primary['attributes']:
-                                address = primary['attributes']['ipv4_address']
-                                if address == meta['ipv4_address']:
-                                    tainted.append((module['path'][-1], key))
+        if returncode != 0 and 'permission denied' in err.lower():
+            to_taint = get_modules_to_taint(host, name)
 
     # if any hosts are tainted, reapply and exit 1
-    if len(tainted) == 0:
+    if len(to_taint) == 0:
         print("no ssh permission errors found, no reapply required")
         sys.exit(0)
     else:
-        for name, resource in tainted:
+        print("ssh permission errors found, running taint & reapply")
+        for name, resource in to_taint:
             cmd = "terraform taint -module={} {}".format(name, resource)
-            print(cmd)
-            ret_code = subprocess.call(shlex.split(cmd))
-            if ret_code != 0:
+            returncode, _, _ = call_cmd(cmd)
+            if returncode != 0:
                 print("subprocess exited with nonzero")
                 sys.exit(1)
 
-        subprocess.call(shlex.split("terraform apply"))
-        sys.exit(0)
-except subprocess.CalledProcessError as e:
-    print("error calling subprocess:\n", e)
-    sys.exit(1)
-except IOError as e:
-    print("error opening file:\n", e)
-    sys.exit(1)
+        call_cmd("terraform apply")
+        sys.exit(1)
+
+
+def call_cmd(cmd):
+    print("RUNNING\t{}".format(cmd))
+    cmd = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
+
+    out, err = cmd.communicate()
+
+    return (cmd.returncode, out, err)
+
+
+def get_modules_to_taint(host, name):
+    to_taint = []
+    with open('terraform.tfstate') as json_file:
+        cmd = "plugins/inventory/terraform.py --host {}".format(name)
+        _, meta, _ = call_cmd(cmd)
+
+        meta = json.loads(meta)
+        state = json.load(json_file)
+
+        for module in state['modules']:
+            for key, resource in module['resources'].items():
+                primary = resource['primary']
+                if 'attributes' in primary:
+                    if 'ipv4_address' in primary['attributes']:
+                        address = primary['attributes']['ipv4_address']
+                        if address == meta['ipv4_address']:
+                            to_taint.append((module['path'][-1], key))
+
+
+if __name__ == "__main__":
+    main()
